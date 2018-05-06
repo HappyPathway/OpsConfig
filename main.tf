a6f3ecf024f5993cc59c73d4fe7ff964dec05692
Dave Arnold <darnold@l-sfh3k1htdd-m.gapinc.gap.com>
2018-05-06 15:06:07 -0700

    adding env to datadog playbook

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "null_resource" "ops_config" {
   provisioner "remote-exec" {
     inline = [
       "curl ${var.ddog_install_script} | sudo DD_API_KEY=${var.datadog_key} bash",
-      "sudo ansible-playbook /tmp/playbooks/datadog_agent.yaml -e datadog_api_key=${var.datadog_key} -e service_name=${var.service_name}",
+      "sudo ansible-playbook /tmp/playbooks/datadog_agent.yaml -e datadog_api_key=${var.datadog_key} -e env=${var.env} -e service_name=${var.service_name}",
       "sudo ansible-playbook /tmp/playbooks/consul_agent.yaml -c local -e env=${var.env} -e consul_cluster=${var.consul_cluster} -e azure_subscription=${var.azure_subscription} -e azure_tenant=${var.azure_tenant} -e azure_client=${var.azure_client} -e azure_secret=${var.azure_secret} -e service_name=${var.service_name} -e service_port=${var.service_port}",
       "rm -rf /tmp/playbooks"
     ]
