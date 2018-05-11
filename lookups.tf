8ebe4f00649cfa38e2cb0efdc974efc93887ce32
Dave Arnold <dja+djaboxx@sonic.net>
2018-05-10 17:56:40 -0700

    Update lookups.tf

--- lookups.tf
+++ lookups.tf
@@ -7,55 +7,55 @@ data "consul_keys" "aws_infra" {
   
   key {
     name    = "subnet_id"
-    path    = "services/${var.env}/${var.service_name}/subnet_id"
+    path    = "environments/${var.env}/${var.service_name}/subnet_id"
     default = ""
   }
   
   key {
     name    = "security_group"
-    path    = "services/${var.env}/${var.service_name}/security_group"
+    path    = environments/${var.env}/${var.service_name}/security_group"
     default = ""
   }
   
   key {
     name    = "ami"
-    path    = "services/${var.service_name}/ami"
+    path    = "environments/${var.service_name}/ami"
     default = ""
   }
 
   key {
     name    = "instance_type"
-    path    = "services/${var.env}/${var.service_name}/instance_type"
+    path    = "environments/${var.env}/${var.service_name}/instance_type"
     default = ""
   }
 
   key {
     name    = "key_pair"
-    path    = "services/${var.env}/${var.service_name}/key_pair"
+    path    = "environments/${var.env}/${var.service_name}/key_pair"
     default = ""
   }
 
   key {
     name    = "instance_user"
-    path    = "services/${var.env}/${var.service_name}/instance_user"
+    path    = "environments/${var.env}/${var.service_name}/instance_user"
     default = ""
   }
 
   key {
     name    = "dns_name"
-    path    = "services/${var.env}/${var.service_name}/dns_name"
+    path    = "environments/${var.env}/${var.service_name}/dns_name"
     default = ""
   }
 
   key {
     name    = "dns_zone"
-    path    = "services/${var.env}/${var.service_name}/dns_zone"
+    path    = "environments/${var.env}/${var.service_name}/dns_zone"
     default = ""
   }
 
   key {
     name    = "private_zone"
-    path    = "services/${var.env}/${var.service_name}/private_zone"
+    path    = "environments/${var.env}/${var.service_name}/private_zone"
     default = ""
   }
 }
